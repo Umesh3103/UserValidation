17457a2c3a7fe611e5c89abd6835b8bb473ce64d
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-30 18:53:39 +0530

    [Umesh]Added: checking validity of first name last name email mobile number password

--- src/main/java/com/learning/UserValidation.java
+++ src/main/java/com/learning/UserValidation.java
@@ -1,8 +1,44 @@
 package com.learning;
 
-public class UserValidation {
+import java.util.regex.Pattern;
 
+public class UserValidation {
+	private static final Pattern FIRST_NAME = Pattern.compile("^[A-Z]{1}[a-z]{2,}");
+	private static final Pattern LAST_NAME = Pattern.compile("[A-Z]{1}[a-z]{2,}");
+	private static final Pattern EMAIL = Pattern.compile("^[0-9 a-z A-Z]+([_+-.]{0,1}[0-9 a-z A-Z]+[@][0-9 a-z A-Z]+[.][a-z A-Z]{2,4})([.][a-z A-Z]{2,4}){0,1}");
+	private static final Pattern MOBILE_NUMBER = Pattern.compile("^[0-9]{2}\\s[6-9]{1}[0-9]{9}");
+	private static final Pattern PASSWORD = Pattern.compile("^(?=.*[A-Z])(?=.*[0-9])(?=.*[_+-.@#$])([a-z A-Z 0-9]).{5,}");
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to User Validation problem");
+		System.out.println("Welcome to User Validation Problem");
 	}
+	public String AnalyseMood(String mood){
+		if(mood.contains("sad")){
+			return "SAD";
+		}
+		else{
+			return "HAPPY";
+		}
+	}
+	public boolean checkFirstName(String name) {
+        return FIRST_NAME.matcher(name).matches();
+    }
+	
+	public boolean checkLastName(String name) {
+        return LAST_NAME.matcher(name).matches();
+    }
+
+	public boolean checkEmail(String name) {
+        return EMAIL.matcher(name).matches();
+    }
+	
+	public boolean checkMobNum(String name) {
+        return MOBILE_NUMBER.matcher(name).matches();
+    }
+	
+	public boolean checkPassword(String name) {
+        return PASSWORD.matcher(name).matches();
+    }
+
 }
+
--- src/test/java/com/learning/UserValidationTest.java
+++ src/test/java/com/learning/UserValidationTest.java
@@ -1,7 +1,122 @@
 package com.learning;
 
+import org.junit.Test;
 
+import org.junit.Assert;
+
+@SuppressWarnings("deprecation")
 public class UserValidationTest {
+
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad(){
+		UserValidation validator = new UserValidation();
+		String mood = validator.AnalyseMood("This is a sad message");
+		Assert.assertEquals("SAD", mood);
+	}
 	
+	@Test
+	public void givenMessage_WhenNotSad_ShouldReturnHappy(){
+		UserValidation validator = new UserValidation();
+		String mood = validator.AnalyseMood("This is a happy message");
+		Assert.assertEquals("HAPPY", mood);
+	}
 	
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkFirstName("Umesh");
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenFirstName_WhenShort_ShouldReturnFalse(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkFirstName("Um");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFalse(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkFirstName("Um@12esh");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkLastName("Deora");
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenLastName_WhenShort_ShouldReturnFalse(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkLastName("De");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void givenLastName_WhenNotProper_ShouldReturnFalse(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkLastName("De12#%ora");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void givenEmail_WhenProper_ShouldReturnTrue(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkEmail("abc@gmail.com");
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenEmail_WhenNotProper_ShouldReturnFalse(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkEmail("abc.@gmail.com");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void givenEmail_WhenOptionalPart1_ShouldReturnTrue(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkEmail("abc.xyz@gmail.com");
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenEmail_WhenOptionalPart2_ShouldReturnTrue(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkEmail("abc@gmail.com.in");
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenMobileNumber_WhenProper_ShouldReturnTrue(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkMobNum("91 9983910255");
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenMobileNumber_WhenNotProper_ShouldReturnFalse(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkMobNum("9123 9983910255");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void givenPassword_WhenProper_ShouldReturnTrue(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkPassword("Umesh@123");
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenPassword_WhenNotProper_ShouldReturnFalse(){
+		UserValidation validator = new UserValidation();
+		boolean result = validator.checkPassword("umesh@123");
+		Assert.assertFalse(result);
+	}
 }
+
